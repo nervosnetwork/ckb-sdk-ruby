471617252a49de223d7e2fdabc91afe28d4fa5e2
James Chen <james@ashchan.com>
2019-06-04 11:57:14 +0900

    feat: Remove Input#block_number which was not used

--- lib/ckb/types/input.rb
+++ lib/ckb/types/input.rb
@@ -3,20 +3,17 @@
 module CKB
   module Types
     class Input
-      attr_reader :block_number, :previous_output, :since
+      attr_reader :previous_output, :since
 
       # @param previous_output [CKB::Types::OutPoint]
       # @param since [String]
-      # @param block_number [String]
-      def initialize(previous_output:, since: "0", block_number: "0")
+      def initialize(previous_output:, since: "0")
         @previous_output = previous_output
         @since = since.to_s
-        @block_number = block_number.to_s
       end
 
       def to_h
         {
-          block_number: block_number,
           previous_output: @previous_output.to_h,
           since: since
         }
@@ -26,7 +23,6 @@ module CKB
         return if hash.nil?
 
         new(
-          block_number: hash[:block_number],
           previous_output: OutPoint.from_h(hash[:previous_output]),
           since: hash[:since]
         )
