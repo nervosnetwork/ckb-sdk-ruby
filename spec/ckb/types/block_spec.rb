159c8ed6041258574f6b89cf8394d9e442300e20
shaojunda <shaojunda@gmail.com>
2019-08-13 15:13:16 +0800

    chore: adjust tests

--- spec/ckb/types/block_spec.rb
+++ spec/ckb/types/block_spec.rb
@@ -3,65 +3,53 @@ RSpec.describe CKB::Types::Block do
     {
       "transactions": [
         {
-          "deps": [
-            "block_hash": "0x0000000000000000000000000000000000000000000000000000000000000000",
-            "cell": {
-              "tx_hash": "0x0000000000000000000000000000000000000000000000000000000000000000",
-              "index": "4294967295"
-            }
-          ],
-          "hash": "0xbd9ed8dec5288bdeb2ebbcc4c118a8adb6baab07a44ea79843255ccda6c57915",
-          "inputs": [
-            {
-              "args": [
-                "0x0100000000000000"
-              ],
-              "previous_output": {
-                "block_hash": nil,
-                "cell": {
-                  "tx_hash": "0x0000000000000000000000000000000000000000000000000000000000000000",
-                  "index": "4294967295"
-                }
-              },
-              "since": "0"
-            }
-          ],
-          "outputs": [
-            {
-              "capacity": "50000",
-              "data": "0x",
-              "lock": {
-                "args": [],
-                "code_hash": "0x0000000000000000000000000000000000000000000000000000000000000001"
-              },
-              "type": nil
-            }
-          ],
-          "version": "0",
-          "witnesses": [
-            {
-                data: "0x1234"
-            }
-          ]
-        }
-      ],
-      "header": {
-        "difficulty": "0x100",
-        "hash": "0xef285e5da29247ce39385cbd8dc36535f7ea1b5b0379db26e9d459a8b47d0d71",
-        "number": "1",
-        "parent_hash": "0xf17b8bfe49aaa018610d20a19aa6a0639882a774c47bcb7623a085a59ee13d42",
-        "dao": "0x0100000000000000747d1c3822f724000053285dc3820400007d3449d6740000",
-        "seal": {
-          "nonce": "14785007515249450415",
-          "proof": "0xa00600005a0a00001c21000009230000db240000fb350000523600005f4b0000bb4b00000a4d00001b56000070700000"
+          "cell_deps":[],
+          "hash":"0xde27dcbd81dd174ede0749df02a883d78215b2f20d7ec8ff395ed220872b764c",
+          "header_deps":[],
+          "inputs":[{
+            "previous_output":{
+              "index":"4294967295",
+              "tx_hash":"0x0000000000000000000000000000000000000000000000000000000000000000"
+            },
+          "since":"15"
+          }],
+          "outputs":[{
+            "capacity":"131540290464",
+            "data_hash":"0x0000000000000000000000000000000000000000000000000000000000000000",
+            "lock":{
+              "args":["0x3954acece65096bfa81258983ddb83915fc56bd8"],
+              "code_hash":"0xa76801d09a0eabbfa545f1577084b6f3bafb0b6250e7f5c89efcfd4e3499fb55",
+              "hash_type":"Data"
+            },
+            "type":nil
+          }],
+          "outputs_data":["0x"],
+          "version":"0",
+          "witnesses":[{
+            "data":[
+              "0xa76801d09a0eabbfa545f1577084b6f3bafb0b6250e7f5c89efcfd4e3499fb5500",
+              "0x3954acece65096bfa81258983ddb83915fc56bd8"
+            ]
+          }]
+       }],
+      "header":{
+        "dao":"0x010000000000000055309658678823000086285987fe0300008f6b7c5f6f0000",
+        "difficulty":"0x100",
+        "epoch":"0",
+        "hash":"0x7ba460895460baa5b2a698bc3d400d33afbb64a0312ed57b1496304c011ade7a",
+        "number":"15",
+        "parent_hash":"0xe1a4178bc36e6abb4d3a5f00b9bac8b6e8e1e2f9b46b160e1d55b480e6247a1a",
+        "proposals_hash":"0x0000000000000000000000000000000000000000000000000000000000000000",
+        "seal":{
+          "nonce":"2058764368806501667",
+          "proof":"0x"
         },
-        "timestamp": "1555422499746",
-        "transactions_root": "0xbd9ed8dec5288bdeb2ebbcc4c118a8adb6baab07a44ea79843255ccda6c57915",
-        "proposals_hash": "0x0000000000000000000000000000000000000000000000000000000000000000",
-        "uncles_count": "0",
-        "uncles_hash": "0x0000000000000000000000000000000000000000000000000000000000000000",
-        "version": "0",
-        "witnesses_root": "0x0000000000000000000000000000000000000000000000000000000000000000"
+        "timestamp":"1565665851354",
+        "transactions_root":"0xde27dcbd81dd174ede0749df02a883d78215b2f20d7ec8ff395ed220872b764c",
+        "uncles_count":"0",
+        "uncles_hash":"0x0000000000000000000000000000000000000000000000000000000000000000",
+        "version":"0",
+        "witnesses_root":"0x3b84ab9739349ec9ba1bfb7da2662d3cb2f1a1e3ecc0470542fbf4b0bee25f67"
       },
       "proposals": [],
       "uncles": []
