d0e4b152d66cc56b5905645309e923eeba068d93
shaojunda <shaojunda@gmail.com>
2019-08-13 15:11:36 +0800

    test: CellDep from_h

---
+++ spec/ckb/types/cell_dep_spec.rb
@@ -0,0 +1,13 @@
+RSpec.describe CKB::Types::CellDep do
+  let(:cell_dep_h) do
+    {
+      out_point: { tx_hash: "0x716842f09fa3022707bb7ad6a4f48eae2d886b3e6414d9e2e2aa8422510fe871", index: "0" },
+      is_group: false
+    }
+  end
+
+  it "from_h" do
+    cell_dep = CKB::Types::CellDep.from_h(cell_dep_h)
+    expect(cell_dep).to be_a(CKB::Types::CellDep)
+  end
+end
