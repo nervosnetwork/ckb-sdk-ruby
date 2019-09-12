8b47dcbcfc8784f7dc352d63c218235c749af37b
shaojunda <shaojunda@gmail.com>
2019-09-12 14:57:29 +0800

    feat: add cell info

---
+++ lib/ckb/types/cell_info.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module CKB
+  module Types
+    class CellInfo
+      attr_accessor :output, :data
+
+      # @param output [CKB::Types::Output]
+      # @param data [CKB::Types::CellData | nil]
+      def initialize(output: nil, data:)
+        @output = output
+        @data = data
+      end
+
+      def to_h
+        {
+          output: output.to_h,
+          data: data.to_h
+        }
+      end
+
+      def self.from_h(hash)
+        return if hash.nil?
+
+        output = Output.from_h(hash[:output])
+        data = CellData.from_h(hash[:data])
+
+        new(
+          output: output,
+          data: data
+        )
+      end
+    end
+  end
+end
