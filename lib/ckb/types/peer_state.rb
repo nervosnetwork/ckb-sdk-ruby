b14b0fbec3e766a72cc1a4ef601dd017d94c3663
shaojunda <shaojunda@gmail.com>
2019-09-16 17:22:21 +0800

    chore: use correct param name

--- lib/ckb/types/peer_state.rb
+++ lib/ckb/types/peer_state.rb
@@ -6,7 +6,7 @@ module CKB
       attr_accessor :last_updated, :blocks_in_flight, :peer
 
       # @param last_updated [String | Integer] timestamp
-      # @param block_in_flight [String | Integer] integer or hex number
+      # @param blocks_in_flight [String | Integer] integer or hex number
       # @param peer [String | Integer] integer or hex number
       def initialize(last_updated:, blocks_in_flight:, peer:)
         @last_updated = Utils.to_int(last_updated)
