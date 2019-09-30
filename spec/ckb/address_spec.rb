880622a24e52597efd34b901fc37f6cbb2f2130c
shaojunda <shaojunda@gmail.com>
2019-09-30 16:28:56 +0800

    chore: remove require pry

--- spec/ckb/address_spec.rb
+++ spec/ckb/address_spec.rb
@@ -1,4 +1,3 @@
-require 'pry'
 RSpec.describe CKB::Address do
   let(:privkey) { "0xe79f3207ea4980b7fed79956d5934249ceac4751a4fae01a0f7c4a96884bc4e3" }
   let(:pubkey) { "0x024a501efd328e062c8675f2365970728c859c592beeefd6be8ead3d901330bc01" }
