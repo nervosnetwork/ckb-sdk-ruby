65f76c48ce91752442aa68dc57af0a9a7061ac9e
classicalliu <classicalliu@gmail.com>
2019-12-14 18:19:15 +0800

    feat: update `get_cells_by_lock_hash` and `get_live_cells_by_lock` returns

--- spec/ckb/types/live_cell_spec.rb
+++ spec/ckb/types/live_cell_spec.rb
@@ -11,10 +11,12 @@ RSpec.describe CKB::Types::LiveCell do
         "type": nil
     },
     "created_by": {
-        "block_number": "0x1",
-        "index": "0x0",
-        "tx_hash": "0x5ba156200c6310bf140fbbd3bfe7e8f03d4d5f82b612c1a8ec2501826eaabc17"
-      }
+      "block_number": "0x1",
+      "index": "0x0",
+      "tx_hash": "0x5ba156200c6310bf140fbbd3bfe7e8f03d4d5f82b612c1a8ec2501826eaabc17"
+    },
+    "cellbase": false,
+    "output_data_len": "0x0"
     }
   end
 
@@ -24,6 +26,8 @@ RSpec.describe CKB::Types::LiveCell do
     expect(live_cell).to be_a(CKB::Types::LiveCell)
     expect(live_cell.cell_output).to be_a(CKB::Types::Output)
     expect(live_cell.created_by).to be_a(CKB::Types::TransactionPoint)
+    expect(live_cell.cellbase).to eq live_cell_h[:cellbase]
+    expect(live_cell.output_data_len).to eq live_cell_h[:output_data_len].hex
   end
 
   it "to_h" do
