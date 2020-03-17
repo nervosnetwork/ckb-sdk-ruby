fbad793e3b729007a59d2af6f52a6add4a6d1a78
shaojunda <shaojunda@gmail.com>
2020-03-17 16:40:01 +0800

    chore: add any one can pay lock

--- lib/ckb/sudt_wallet.rb
+++ lib/ckb/sudt_wallet.rb
@@ -2,10 +2,13 @@ module CKB
   class SUDTWallet
     SUDT_CODE_HASH = "0x48dbf59b4c7ee1547238021b4869bceedf4eea6b43772e5d66ef8865b6ae7212"
     SUDT_OUT_POINT_TX_HASH = "0xa608b247bf322474f0b9cb3b6928eff08ecffac658724bcae4611a37d0da7218"
-    attr_reader :api, :key, :blake160, :address, :sudt_type_script_cell_dep, :sudt_out_point
+    EVERYONE_CAN_PAY_CODE_HAHS= "0xed9d2fd9969979f44b9686645099781279c26a7bef358d8f2e75a71de161b1e2"
+    attr_reader :api, :key, :blake160, :address, :sudt_type_script_cell_dep, :sudt_out_point, :min_capacity, :min_amount, :anyone_can_pay_address
 
-    def initialize(api:, key:, mode: MODE::TESTNET)
+    def initialize(api:, key:, mode: MODE::TESTNET, min_capacity: nil, min_amount: nil)
       @api = api
+      @min_capacity = min_capacity
+      @min_amount = min_amount
       if key.is_a?(CKB::Key)
         @key = key
         @pubkey = @key.pubkey
@@ -15,6 +18,7 @@ module CKB
       end
       @blake160 = CKB::Key.blake160(@pubkey)
       @address = Address.new(lock, mode: mode).to_s
+      @anyone_can_pay_address = Address.new(anyone_can_pay_lock, mode: mode).to_s
       ## TODO need change to production config
       @sudt_out_point = Types::OutPoint.new(tx_hash: SUDT_OUT_POINT_TX_HASH, index: 0)
       @sudt_type_script_cell_dep = Types::CellDep.new(out_point: @sudt_out_point)
@@ -215,7 +219,7 @@ module CKB
         api,
         skip_data_and_type: true
       ).gather_inputs(
-        [lock.compute_hash],
+        [lock_hash],
         capacity,
         min_change_capacity,
         fee
@@ -253,7 +257,6 @@ module CKB
     def get_unspent_sudt_cells(type_hash = nil, need_amounts = nil, need_capacities = nil)
       raise "type_hash can't be empty" if type_hash.nil? || type_hash.empty?
 
-      lock_hash = lock.compute_hash
       to = api.get_tip_block_number.to_i
       results = []
       total_amount = 0
@@ -299,11 +302,36 @@ module CKB
       }
     end
 
+    def anyone_can_pay_lock
+      pubkey_hash = blake160
+      if min_capacity
+        pubkey_hash = CKB::Utils.hex_concat(blake160, [min_capacity].pack("C"))
+      end
+      if min_amount
+        pubkey_hash = CKB::Utils.hex_concat(blake160, [min_amount].pack("C"))
+      end
+      Types::Script.generate_lock(
+        pubkey_hash,
+        EVERYONE_CAN_PAY_CODE_HAHS
+      )
+    end
+
     def lock
       Types::Script.generate_lock(
         blake160,
         SystemCodeHash::SECP256K1_BLAKE160_SIGHASH_ALL_TYPE_HASH
       )
+    end
+
+    def lock_hash
+      @lock_hash ||=
+        begin
+          if min_capacity
+            anyone_can_pay_lock.compute_hash
+          else
+            lock.compute_hash
+          end
+        end
     end
 
     def get_key(key)
@@ -319,7 +347,7 @@ module CKB
 
     def sudt_type_script
       ## TODO hash_type will change to type
-      Types::Script.new(code_hash: SUDT_CODE_HASH, args: lock.compute_hash, hash_type: "data")
+      Types::Script.new(code_hash: SUDT_CODE_HASH, args: lock_hash, hash_type: "data")
     end
 
     def parse_udt_cell_data(data)
