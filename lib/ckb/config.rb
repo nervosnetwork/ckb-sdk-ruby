622131727d860a98783f4fafb53eacd0744772dc
shaojunda <shaojunda@gmail.com>
2020-03-25 00:42:26 +0800

    chore: add default cell dep to config

--- lib/ckb/config.rb
+++ lib/ckb/config.rb
@@ -6,9 +6,17 @@ module CKB
     include Singleton
     attr_accessor :lock_handlers, :type_handlers
 
+    # https://github.com/nervosnetwork/ckb/blob/develop/resource/specs/mainnet.toml#L73
+    SECP256K1_BLAKE160_SIGHASH_ALL_TYPE_HASH = "0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8"
+    # https://github.com/nervosnetwork/ckb/blob/develop/resource/specs/mainnet.toml#L127
+    SECP256K1_BLAKE160_MULTISIG_ALL_TYPE_HASH = "0x5c5069eb0857efc65e1bca0c07df34c31663b3622fd3876c876320fc9634e2a8"
+    SECP256K1_GROUP_CELL_TRANSACTION_HASH = "0x71a7ba8fc96349fea0ed3a5c47992e3b4084b031a42264a018e0072e8172e46c"
+    HASH_TYPES = [TYPE = "type", DATA = "data"]
+
+
     def initialize
       @lock_handlers = {
-        [CKB::SystemCodeHash::SECP256K1_BLAKE160_SIGHASH_ALL_TYPE_HASH, CKB::ScriptHashType::TYPE] => CKB::SingleSign
+        [SECP256K1_BLAKE160_SIGHASH_ALL_TYPE_HASH, TYPE] => CKB::SingleSigner
       }
       @type_handlers = {}
     end
@@ -19,6 +27,13 @@ module CKB
 
     def type_handler(type_script)
       type_handlers[[type_script.code_hash, type_script.hash_type]]
+    end
+
+    def standard_secp256k1_blake160_sighash_all_cell_dep
+      CKB::Types::CellDep.new(
+        out_point: CKB::Types::OutPoint.new(tx_hash: SECP256K1_GROUP_CELL_TRANSACTION_HASH, index: 0),
+        dep_type: "dep_group"
+      )
     end
   end
 end
