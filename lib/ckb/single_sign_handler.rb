69b5014061beb0bfc9a8c3e67c9d185323b0c730
shaojunda <shaojunda@gmail.com>
2020-03-25 20:09:15 +0800

    chore: add sign method to single sign handler

--- lib/ckb/single_sign_handler.rb
+++ lib/ckb/single_sign_handler.rb
@@ -8,13 +8,34 @@ module CKB
       cell_dep = CKB::Config.instance.standard_secp256k1_blake160_sighash_all_cell_dep
       tx_generator.transaction.cell_deps << cell_dep unless tx_generator.transaction.cell_deps.include?(cell_dep)
       witness =
-        if tx_generator.cell_metas.any? { |inner_cell_meta| inner_cell_meta.output.lock == cell_meta.output.lock }
+        if tx_generator.cell_metas.any? { |inner_cell_meta| inner_cell_meta.output.lock.compute_hash == cell_meta.output.lock.compute_hash }
           CKB::Types::Witness.new
         else
           CKB::Types::Witness.new(lock: "0x#{'0' * 130}")
         end
       tx_generator.transaction.witnesses << witness
       tx_generator.cell_metas << cell_meta
+    end
+
+    def sign(cell_meta:, tx_generator:, context:)
+      lock_script = cell_meta.output.lock_script
+      cell_meta_index = tx_generator.cell_metas.find_index { |inner_cell_meta| inner_cell_meta == cell_meta  }
+      grouped_indexes = tx_generator.cell_metas.map.with_index { |inner_cell_meta, index| index if inner_cell_meta.output.lock.compute_hash == lock_script.compute_hash }.compact
+      if cell_meta_index == grouped_indexes.first
+        transaction = tx_generator.transaction
+        blake2b = CKB::Blake2b.new
+        blake2b << transaction.compute_hash
+        grouped_indexes.each do |index|
+          witness = tx_generator.witness[index]
+          binary_witness = Utils.hex_to_bin(CKB::Serializers::WitnessArgsSerializer.new(witness).serialize)
+          binary_witness_size = binary_witness.bytesize
+          blake2b << [binary_witness_size].pack("Q<")
+          blake2b << binary_witness
+        end
+        message = blake2b.hexdigest
+        private_key = CKB::Key.new(context)
+        tx_generator.witnesses[cell_meta_index].lock = private_key.sign_recoverable(message)
+      end
     end
   end
 end
