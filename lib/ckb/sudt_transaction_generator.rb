7024f8da0dd006e0254fbbf4f73201f83627ae33
shaojunda <shaojunda@gmail.com>
2020-04-04 02:04:52 +0800

    refactor: move generate sudt amount to utils

--- lib/ckb/sudt_transaction_generator.rb
+++ lib/ckb/sudt_transaction_generator.rb
@@ -9,21 +9,17 @@ module CKB
         if is_issuer
           true
         else
-          inputs_sudt_amount = cell_metas.select { |cell_meta| !cell_meta.output.type.nil? }.map { |cell_meta| sudt_amount(cell_meta.output_data) }.sum
-          outputs_sudt_amount = transaction.outputs_data.map { |output_data| sudt_amount(output_data) }.sum
+          inputs_sudt_amount = cell_metas.select { |cell_meta| !cell_meta.output.type.nil? }.map { |cell_meta| CKB::Utils.sudt_amount(cell_meta.output_data) }.sum
+          outputs_sudt_amount = transaction.outputs_data.map { |output_data| CKB::Utils.sudt_amount(output_data) }.sum
           change_sudt_amount = inputs_sudt_amount - outputs_sudt_amount
           if change_sudt_amount >= 0
-            data = [change_sudt_amount].pack("Q<*") + [change_sudt_amount >> 64].pack("Q<*")
-            transaction.outputs_data[change_output_index] = CKB::Utils.bin_to_hex(data)
+            data = CKB::Utils.generate_sudt_amount(change_sudt_amount)
+            transaction.outputs_data[change_output_index] = data
 
             true
           end
         end
       end
-    end
-
-    def sudt_amount(output_data)
-      CKB::Utils.hex_to_bin(output_data).reverse.unpack1("B*").to_i(2)
     end
   end
 end
