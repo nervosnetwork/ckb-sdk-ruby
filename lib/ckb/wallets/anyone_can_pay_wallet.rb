2c69fbf916a3897c5cf7c2f74f10ff75b6da7cf1
shaojunda <shaojunda@gmail.com>
2020-04-09 03:20:29 +0800

    chore: support owner consume cell

--- lib/ckb/wallets/anyone_can_pay_wallet.rb
+++ lib/ckb/wallets/anyone_can_pay_wallet.rb
@@ -4,7 +4,7 @@ module CKB
   module Wallets
     class AnyoneCanPayWallet < NewWallet
       attr_reader :sudt_type_script, :sudt_args
-      attr_accessor :anyone_can_pay_cell_lock_scripts, :need_sudt
+      attr_accessor :anyone_can_pay_cell_lock_scripts, :need_sudt, :is_owner
 
       def initialize(api:, from_addresses:, anyone_can_pay_addresses:, sudt_args:, collector_type: :default_scanner, mode: MODE::TESTNET, from_block_number: 0)
         super(api: api, from_addresses: from_addresses, collector_type: collector_type, mode: mode, from_block_number: from_block_number)
@@ -16,6 +16,7 @@ module CKB
 
           script
         end
+        @is_owner = input_scripts.size == 1 && input_scripts.first.compute_hash == anyone_can_pay_cell_lock_scripts.first.compute_hash
         @need_sudt = true
       end
 
@@ -28,7 +29,7 @@ module CKB
           data = "0x#{'0' * 32}"
           @need_sudt = false
         when :udt
-          capacity = 0
+          capacity = is_owner ? CKB::Utils.byte_to_shannon(142) : 0
           data = CKB::Utils.generate_sudt_amount(transfer_amount)
         else
           raise "wrong transfer type, only support ckb or udt"
@@ -55,12 +56,14 @@ module CKB
           outputs_data << (output_info[:data] || "0x")
         end
 
-        if outputs.all? { |output| output.capacity > 0 } || (outputs.map { |output| output.capacity }.sum == 0 && outputs.size == 1)
+        if outputs.all? { |output| output.capacity > 0 } || (outputs.map { |output| output.capacity }.sum == 0)
           if to_infos.any? { |_, output_info| output_info[:type] && output_info[:type].compute_hash == sudt_type_script.compute_hash }
             outputs << CKB::Types::Output.new(capacity: 0, lock: input_scripts[-1], type: nil)
             outputs_data << "0x"
-            outputs << CKB::Types::Output.new(capacity: CKB::Utils.byte_to_shannon(142), lock: input_scripts[-1], type: sudt_type_script)
-            outputs_data << "0x#{'0' * 32}"
+            if need_sudt
+              outputs << CKB::Types::Output.new(capacity: CKB::Utils.byte_to_shannon(142), lock: input_scripts[-1], type: sudt_type_script)
+              outputs_data << "0x#{'0' * 32}"
+            end
           else
             outputs << CKB::Types::Output.new(capacity: 0, lock: input_scripts[-1], type: nil)
             outputs_data << "0x"
@@ -75,8 +78,8 @@ module CKB
         tx_generator = CKB::AnyoneCanPayTransactionGenerator.new(api, transaction)
         tx_generator.need_sudt = need_sudt
         tx_generator.sudt_args = sudt_args
+        tx_generator.is_owner = is_owner
         tx_generator.anyone_can_pay_cell_lock_scripts = anyone_can_pay_cell_lock_scripts
-
         tx_generator.generate(anyone_can_pay_collector: anyone_can_pay_collector, udt_collector: udt_collector, capacity_collector: capacity_collector, contexts: input_scripts.map(&:compute_hash).zip(contexts).to_h, fee_rate: fee_rate)
 
         tx_generator
@@ -85,6 +88,8 @@ module CKB
       private
 
       def sender_cell?(cell_meta)
+        return true if is_owner
+
         sender_scripts = input_scripts.reject { |script| anyone_can_pay_cell_lock_scripts.map(&:compute_hash).include?(script.compute_hash) }
         sender_scripts.map(&:compute_hash).include?(cell_meta.output.lock.compute_hash)
       end
