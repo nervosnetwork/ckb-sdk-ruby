d208f1f5be29e31c6dd8cde5a8eed9a35e55ecab
shaojunda <shaojunda@gmail.com>
2020-08-20 22:10:04 +0800

    feat: add tip info to tx_pool_info RPC

--- lib/ckb/types/tx_pool_info.rb
+++ lib/ckb/types/tx_pool_info.rb
@@ -3,7 +3,7 @@
 module CKB
   module Types
     class TxPoolInfo
-      attr_accessor :pending, :proposed, :orphan, :total_tx_cycles, :total_tx_size, :last_txs_updated_at, :min_fee_rate
+      attr_accessor :pending, :proposed, :orphan, :total_tx_cycles, :total_tx_size, :last_txs_updated_at, :min_fee_rate, :tip_hash, :tip_number
 
       # @param pending [String | Integer] integer or hex number
       # @param proposed [String | Integer] integer or hex number
@@ -11,6 +11,8 @@ module CKB
       # @param total_tx_cycles [String | Integer] integer or hex number
       # @param total_tx_size [String | Integer] integer or hex number
       # @param last_txs_updated_at [String | Integer] timestamp
+      # @param tip_hash [String] 0x...
+      # @param tip_number [String | Integer] integer or hex number
       def initialize(
         pending:,
         proposed:,
@@ -18,7 +20,9 @@ module CKB
         last_txs_updated_at:,
         min_fee_rate:,
         total_tx_cycles:,
-        total_tx_size:
+        total_tx_size:,
+        tip_hash:,
+        tip_number:
       )
         @pending = Utils.to_int(pending)
         @proposed = Utils.to_int(proposed)
@@ -27,6 +31,8 @@ module CKB
         @total_tx_size = Utils.to_int(total_tx_size)
         @last_txs_updated_at = Utils.to_int(last_txs_updated_at)
         @min_fee_rate = Utils.to_int(min_fee_rate)
+        @tip_hash = tip_hash
+        @tip_number = Utils.to_int(tip_number)
       end
 
       def to_h
@@ -37,7 +43,9 @@ module CKB
           total_tx_cycles: Utils.to_hex(@total_tx_cycles),
           total_tx_size: Utils.to_hex(@total_tx_size),
           last_txs_updated_at: Utils.to_hex(@last_txs_updated_at),
-          min_fee_rate: Utils.to_hex(@min_fee_rate)
+          min_fee_rate: Utils.to_hex(@min_fee_rate),
+          tip_hash: @tip_hash,
+          tip_number: Utils.to_hex(@tip_number)
         }
       end
 
@@ -51,7 +59,9 @@ module CKB
           total_tx_cycles: hash[:total_tx_cycles],
           total_tx_size: hash[:total_tx_size],
           last_txs_updated_at: hash[:last_txs_updated_at],
-          min_fee_rate: hash[:min_fee_rate]
+          min_fee_rate: hash[:min_fee_rate],
+          tip_hash: hash[:tip_hash],
+          tip_number: hash[:tip_number]
         )
       end
     end
