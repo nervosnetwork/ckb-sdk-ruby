a3228f88b35052145be090e5348215dc5bbe9bc9
shaojunda <shaojunda@gmail.com>
2021-08-02 14:12:34 +0800

    feat: implementing equality for cell_meta

--- lib/ckb/cell_meta.rb
+++ lib/ckb/cell_meta.rb
@@ -16,5 +16,12 @@ module CKB
     def output_data
       @output_data ||= api.get_live_cell(out_point, true).cell.data.content
     end
+
+    def ==(other)
+      self.out_point.to_h == other.out_point.to_h &&
+        self.output.to_h == other.output.to_h &&
+        self.output_data_len == other.output_data_len &&
+        self.cellbase == other.cellbase
+    end
   end
 end
