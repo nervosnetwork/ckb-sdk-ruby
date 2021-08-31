fec87fd62013ce3f40f60d820adbef950768fdb0
shaojunda <shaojunda@gmail.com>
2021-08-31 20:32:54 +0800

    feat(hardfork): add new allowed value 'data1' to hash_type

--- spec/ckb/types/transaction_spec.rb
+++ spec/ckb/types/transaction_spec.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 RSpec.describe CKB::Types::Transaction do
   let(:private_key) { ENV["BOB_PRIVATE_KEY"] }
   let(:key) { CKB::Key.new(private_key) }
@@ -45,9 +46,9 @@ RSpec.describe CKB::Types::Transaction do
           "type": nil
         }
       ],
-      "outputs_data": [
-        "0x",
-        "0x"
+      "outputs_data": %w[
+        0x
+        0x
       ],
       "witnesses": [CKB::Types::Witness.new],
       "hash": "0x8457b932a9a3cd0bedd3eebad473cf7fd1f59cd5f96f80a637271a6901e4a0da"
@@ -164,9 +165,9 @@ RSpec.describe CKB::Types::Transaction do
             "type": nil
           }
         ],
-        "outputs_data": [
-          "0x",
-          "0x"
+        "outputs_data": %w[
+          0x
+          0x
         ],
         "witnesses": [
           CKB::Types::Witness.new,
@@ -184,13 +185,13 @@ RSpec.describe CKB::Types::Transaction do
       tx_to_sign = CKB::Types::Transaction.from_h(tx_to_sign_hash)
       signed_tx = tx_to_sign.sign(key)
       expect(signed_tx.hash).to eq tx_hash
-      expect(signed_tx.to_raw_transaction_h[:witnesses]).to eq([
-        "0x550000001000000055000000550000004100000042ab4a61dec16c0dce4bdec692eb4f9814af4f50ce929cf0b88c4282e2970ce14ee827c7707676f50da13108e8ef23aed9043d7b64f78e216f2bd750888ecbae01",
-        "0x10000000100000001000000010000000",
-        "0x10000000100000001000000010000000",
-        "0x10000000100000001000000010000000",
-        "0x10000000100000001000000010000000"
-      ])
+      expect(signed_tx.to_raw_transaction_h[:witnesses]).to eq(%w[
+                                                                 0x550000001000000055000000550000004100000042ab4a61dec16c0dce4bdec692eb4f9814af4f50ce929cf0b88c4282e2970ce14ee827c7707676f50da13108e8ef23aed9043d7b64f78e216f2bd750888ecbae01
+                                                                 0x10000000100000001000000010000000
+                                                                 0x10000000100000001000000010000000
+                                                                 0x10000000100000001000000010000000
+                                                                 0x10000000100000001000000010000000
+                                                               ])
     end
 
     let(:tx_to_sign_hash_use_data_hash) do
@@ -201,15 +202,17 @@ RSpec.describe CKB::Types::Transaction do
         inputs: [{ previous_output: { tx_hash: "0x91fcfd61f420c1090aeded6b6d91d5920a279fe53ec34353afccc59264eeddd4", index: "0x0" },
                    since: "0x71" },
                  { previous_output: { tx_hash: "0x00000000000000000000000000004e4552564f5344414f494e50555430303031", index: "0x0" },
-                   since: "0x0" }
-        ],
+                   since: "0x0" }],
         outputs: [{ capacity: "0x9184efca682",
                     lock: { code_hash: "0xf1951123466e4479842387a66fabfd6b65fc87fd84ae8e6cd3053edb27fff2fd",
                             args: "0x36c329ed630d6ce750712a477543672adab57f4c",
                             hash_type: "data" },
                     type: nil }],
         outputs_data: %w[0x],
-        witnesses: [CKB::Types::Witness.new(lock: "0x4107bd23eedb9f2a2a749108f6bb9720d745d50f044cc4814bafe189a01fe6fb", input_type: "0x99caa8d7efdaab11c2bb7e45f4f385d0405f0fa2e8d3ba48496c28a2443e607d", output_type: "0xa6d5e23a77f4d7940aeb88764eebf8146185138641ac43b233e1c9b3daa170fa"), "0x"]}
+        witnesses: [
+          CKB::Types::Witness.new(lock: "0x4107bd23eedb9f2a2a749108f6bb9720d745d50f044cc4814bafe189a01fe6fb",
+                                  input_type: "0x99caa8d7efdaab11c2bb7e45f4f385d0405f0fa2e8d3ba48496c28a2443e607d", output_type: "0xa6d5e23a77f4d7940aeb88764eebf8146185138641ac43b233e1c9b3daa170fa"), "0x"
+        ] }
     end
 
     it "sign2" do
@@ -218,7 +221,9 @@ RSpec.describe CKB::Types::Transaction do
       signed_tx = tx_to_sign.sign(key)
 
       expect(signed_tx.to_h[:hash]).to eq(tx_hash)
-      expect(signed_tx.to_h[:witnesses]).to eq(%w(0x9d00000010000000550000007900000041000000d896d67ddda97ab2d15cd13098b40e4a2b6d6c66ad465d987df9a28b0a038f4a18dbebbc702a1a0b2056aa9e4290a3640a4d73dd1f6483e6f8e0cd2784b4a78b002000000099caa8d7efdaab11c2bb7e45f4f385d0405f0fa2e8d3ba48496c28a2443e607d20000000a6d5e23a77f4d7940aeb88764eebf8146185138641ac43b233e1c9b3daa170fa 0x))
+      expect(signed_tx.to_h[:witnesses]).to eq(%w[
+                                                 0x9d00000010000000550000007900000041000000d896d67ddda97ab2d15cd13098b40e4a2b6d6c66ad465d987df9a28b0a038f4a18dbebbc702a1a0b2056aa9e4290a3640a4d73dd1f6483e6f8e0cd2784b4a78b002000000099caa8d7efdaab11c2bb7e45f4f385d0405f0fa2e8d3ba48496c28a2443e607d20000000a6d5e23a77f4d7940aeb88764eebf8146185138641ac43b233e1c9b3daa170fa 0x
+                                               ])
     end
   end
 
@@ -301,28 +306,28 @@ RSpec.describe CKB::Types::Transaction do
 
     let(:has_type_script_tx_h) do
       { version: "0x0",
-                       cell_deps: [{ out_point: { tx_hash: "0xe7d5ddd093bcc5909a6f441882e58906062eaf66a6ac1bcf7d7411931bc9ab72", index: "0x0" },
-                                     dep_type: "dep_group" },
-                                   { out_point: { tx_hash: "0x50b9240d466800a6d0b2d07e10151b3a48f44c801a0f7f22e4e033419bd69b8e", index: "0x2" },
-                                     dep_type: "code" }],
-                       header_deps: [],
-                       inputs: [{ previous_output: { tx_hash: "0x3c515d38292bff5068eeddc94fa4aaea0f60f78a9c565ea3fb5835c8c395febf", index: "0x0" },
-                                  since: "0x0" }],
-                       outputs: [{ capacity: "0x9502f9000",
-                                   lock: { code_hash: "0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8",
-                                           args: "0x59a27ef3ba84f061517d13f42cf44ed020610061",
-                                           hash_type: "type" },
-                                   type: { code_hash: "0x88af2314293c7799a11a8ce3b551fb632d5ffa21b42d04326b051f580ca55f6a",
-                                           args: "0x",
-                                           hash_type: "data" } },
-                                 { capacity: "0xdf8475800",
-                                   lock: { code_hash: "0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8",
-                                           args: "0x59a27ef3ba84f061517d13f42cf44ed020610061",
-                                           hash_type: "type" },
-                                   type: nil }],
-                       outputs_data: %w[0x 0x],
-                       witnesses: ["0x7da70e8d0d529afb5f9193a5063463a757c305a4af0e882f6dbacc7cbe74096f1b14aabac84990e7902f28f40e2c8dd70f8dd06a600185c4e89090c25a7bf61700"],
-                       hash: "0xb4d2204035bf8df2ecec1ef9b632b5fc91a245555a96eb0812ed0b29b3f4039a" }
+        cell_deps: [{ out_point: { tx_hash: "0xe7d5ddd093bcc5909a6f441882e58906062eaf66a6ac1bcf7d7411931bc9ab72", index: "0x0" },
+                      dep_type: "dep_group" },
+                    { out_point: { tx_hash: "0x50b9240d466800a6d0b2d07e10151b3a48f44c801a0f7f22e4e033419bd69b8e", index: "0x2" },
+                      dep_type: "code" }],
+        header_deps: [],
+        inputs: [{ previous_output: { tx_hash: "0x3c515d38292bff5068eeddc94fa4aaea0f60f78a9c565ea3fb5835c8c395febf", index: "0x0" },
+                   since: "0x0" }],
+        outputs: [{ capacity: "0x9502f9000",
+                    lock: { code_hash: "0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8",
+                            args: "0x59a27ef3ba84f061517d13f42cf44ed020610061",
+                            hash_type: "type" },
+                    type: { code_hash: "0x88af2314293c7799a11a8ce3b551fb632d5ffa21b42d04326b051f580ca55f6a",
+                            args: "0x",
+                            hash_type: "data" } },
+                  { capacity: "0xdf8475800",
+                    lock: { code_hash: "0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8",
+                            args: "0x59a27ef3ba84f061517d13f42cf44ed020610061",
+                            hash_type: "type" },
+                    type: nil }],
+        outputs_data: %w[0x 0x],
+        witnesses: ["0x7da70e8d0d529afb5f9193a5063463a757c305a4af0e882f6dbacc7cbe74096f1b14aabac84990e7902f28f40e2c8dd70f8dd06a600185c4e89090c25a7bf61700"],
+        hash: "0xb4d2204035bf8df2ecec1ef9b632b5fc91a245555a96eb0812ed0b29b3f4039a" }
     end
 
     it "should build correct hash for specific transaction" do
@@ -355,6 +360,73 @@ RSpec.describe CKB::Types::Transaction do
       expect(
         tx.compute_hash
       ).to eq tx.hash
+    end
+
+    it "should build correct hash when has data1 cell" do
+      tx_h = {
+        "version": "0x0",
+        "cell_deps": [
+          {
+            "out_point": {
+              "tx_hash": "0xace5ea83c478bb866edf122ff862085789158f5cbff155b7bb5f13058555b708",
+              "index": "0x0"
+            },
+            "dep_type": "dep_group"
+          }
+        ],
+        "header_deps": [],
+        "inputs": [
+          {
+            "since": "0x0",
+            "previous_output": {
+              "tx_hash": "0xa563884b3686078ec7e7677a5f86449b15cf2693f3c1241766c6996f206cc541",
+              "index": "0x7"
+            }
+          }
+        ],
+        "outputs": [
+          {
+            "capacity": "0x2540be400",
+            "lock": {
+              "code_hash": "0x709f3fda12f561cfacf92273c57a98fede188a3f1a59b1f888d113f9cce08649",
+              "hash_type": "data",
+              "args": "0xc8328aabcd9b9e8e64fbc566c4385c3bdeb219d7"
+            },
+            "type": nil
+          },
+          {
+            "capacity": "0x2540be400",
+            "lock": {
+              "code_hash": "0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8",
+              "hash_type": "type",
+              "args": "0xc8328aabcd9b9e8e64fbc566c4385c3bdeb219d7"
+            },
+            "type": nil
+          },
+          {
+            "capacity": "0x2540be400",
+            "lock": {
+              "code_hash": "0x709f3fda12f561cfacf92273c57a98fede188a3f1a59b1f888d113f9cce08649",
+              "hash_type": "data1",
+              "args": "0xc8328aabcd9b9e8e64fbc566c4385c3bdeb219d7"
+            },
+            "type": nil
+          }
+        ],
+        "outputs_data": %w[
+          0x
+          0x
+          0x
+        ],
+        "witnesses": [
+          "0x550000001000000055000000550000004100000070b823564f7d1f814cc135ddd56fd8e8931b3a7040eaf1fb828adae29736a3cb0bc7f65021135b293d10a22da61fcc64f7cb660bf2c3276ad63630dad0b6099001"
+        ]
+      }
+
+      tx = CKB::Types::Transaction.from_h(tx_h)
+      expect(
+        tx.compute_hash
+      ).to eq "0x9110ca9266f89938f09ae6f93cc914b2c856cc842440d56fda6d16ee62543f5c"
     end
   end
 end
